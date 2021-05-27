91ae1fb84ee14e3e6be05444ae0c31ceda4ef004
vbusa1 <vbusa1@jhmi.edu>
2021-05-27 15:44:22 -0400

    Include calculation of background SE, add arguments if do not want to take time to calculate it

--- R/runStereogene.R
+++ R/runStereogene.R
@@ -10,7 +10,9 @@
 #' More information for this can be found in the StereoGene README. Optional
 #' @param confounder Confounder file name; a string. More information for this
 #' can be found in the StereoGene README. Optional
-#' @param nShuffle Permutations used to estimate error. Default 5000.
+#' @param nShuffle Permutations used to estimate error. Default 1000.
+#' @param get_error Whether to calculate the standard error of background
+#' permutations from nShuffle. FALSE will save calculation time. Default TRUE
 #'
 #' @return generates StereoGene output files in directory
 #'
@@ -25,17 +27,15 @@ runStereogene <- function(track_files,
                             name_config,
                             pcorProfile = NULL,
                             confounder = NULL,
-                            nShuffle = 5000) {
-    if (length(track_files) < 2) {
-        stop("Must have at least two track or interval files for correlation.")
+                            nShuffle = 1000,
+                            get_error = TRUE) {
+    if (length(track_files) != 2) {
+        stop("Must have two track or interval files for correlation.")
     }
     if (length(name_config) < 1) {
         stop("Must provide a name for the configuration file.")
     }
-    tracks <- ""
-    for (track in track_files) {
-        tracks <- paste0(tracks, " ", track)
-    }
+    tracks <- paste(track_files, collapse = " ")
     config <- paste0("-cfg ", name_config, " ")
     partial_corr <- ""
     if (length(pcorProfile) == 1) {
@@ -46,6 +46,63 @@ runStereogene <- function(track_files,
         confound <- paste0("-confounder ", confounder, " ")
     }
     if(.is_StereoGene_installed()){
-        .StereoGene_run(paste0(config, partial_corr, confound, tracks))
+        window<- as.numeric(substring(grep("wSize=",
+                                           readLines(name_config),
+                                           value = TRUE), 7))
+        if(get_error == TRUE){
+        Bkg<-matrix(nrow = window, ncol = nShuffle)
+        for(n in 1:nShuffle){
+            .StereoGene_run(paste0("-nShuffle 1 ", config, partial_corr,
+                                   confound, tracks))
+            Bkg[,n]<-read.table(paste0(gsub("\\..*", "", track_files[1]), "~",
+                                       gsub("\\..*", "", track_files[2]),
+                                       ".dist"), header = TRUE)$Bkg
+            if(n > 1){
+                if(Bkg[1,n] == Bkg[1,(n-1)]){ # sometimes get repeats
+                .StereoGene_run(paste0("-nShuffle 1 ", config, partial_corr,
+                                       confound, tracks))
+                Bkg[,n]<-read.table(paste0(gsub("\\..*", "",
+                                                track_files[1]), "~",
+                                           gsub("\\..*", "",
+                                                track_files[2]),
+                                           ".dist"), header = TRUE)$Bkg
+                }
+                }
+        }
+        dist<-data.frame(x = read.table(paste0(gsub("\\..*", "",
+                                                    track_files[1]), "~",
+                                               gsub("\\..*", "",
+                                                    track_files[2]),
+                                               ".dist"), header = TRUE)$x,
+                         Fg = read.table(paste0(gsub("\\..*", "",
+                                                     track_files[1]), "~",
+                                                gsub("\\..*", "",
+                                                     track_files[2]),
+                                                ".dist"), header = TRUE)$Fg,
+                         Bkg = apply(Bkg, 1, mean),
+                         Bkg_se = apply(Bkg, 1, sd)/nShuffle)
+        }else{
+        .StereoGene_run(paste0("-nShuffle ", nShuffle, " ", config,
+                               partial_corr, confound, tracks))
+        dist<-data.frame(x = read.table(paste0(gsub("\\..*", "",
+                                                    track_files[1]), "~",
+                                               gsub("\\..*", "",
+                                                    track_files[2]),
+                                               ".dist"), header = TRUE)$x,
+                         Fg = read.table(paste0(gsub("\\..*", "",
+                                                     track_files[1]), "~",
+                                                gsub("\\..*", "",
+                                                     track_files[2]),
+                                                ".dist"), header = TRUE)$Fg,
+                         Bkg = read.table(paste0(gsub("\\..*", "",
+                                                     track_files[1]), "~",
+                                                gsub("\\..*", "",
+                                                     track_files[2]),
+                                                ".dist"), header = TRUE)$Bkg,
+                         Bkg_se = 0)
+        }
+        write.table(dist, file = paste0(gsub("\\..*", "", track_files[1]), "~",
+                                  gsub("\\..*", "", track_files[2]),
+                                  ".dist"), quote = F)
     }else{return("Please install Stereogene and place in working PATH")}
 }
--- R/runStereogeneOnCapR.R
+++ R/runStereogeneOnCapR.R
@@ -14,7 +14,9 @@
 #' format without a header where first column is chromosome name and second
 #' column is chromosome length, as from getChainChrSize. Required
 #' @param name_config Name of output config file. Default config.cfg
-#' @param nShuffle Number of permutations used to estimate error. Default 5000.
+#' @param nShuffle Number of permutations used to estimate error. Default 100.
+#' @param get_error Whether to calculate the standard error of background
+#' permutations from nShuffle. FALSE will save calculation time. Default TRUE
 #' @param ... includes all other parameters acceptable to write_config and
 #' write_stereogene
 #'
@@ -36,52 +38,23 @@ runStereogeneOnCapR <- function(dir_CapR_bg = ".",
                                 output_prefix = input_prefix,
                                 name_config = "config.cfg",
                                 chrom_size,
-                                nShuffle = 5000,
+                                nShuffle = 100,
+                                get_error = TRUE,
                                 ...) {
     if (missing(chrom_size)){stop("please provide a chrom_size file")}
-    doCall(write_config, args = list(
-        name_config = name_config,
-        chrom_size = chrom_size, ...))
-    track_files <- c(paste0(dir_CapR_bg, "/", input_prefix,
-        "_hairpin_liftOver.bedGraph"), protein_file)
-    name_sh <- paste0(output_prefix, "_stereogene_hairpin.sh")
-    doCall(runStereogene, args = list(...,
-        track_files = track_files,
-        name_sh = name_sh,
-        name_config = name_config))
-    track_files <- c(paste0(dir_CapR_bg, "/", input_prefix,
-        "_internal_liftOver.bedGraph"), protein_file)
-    name_sh <- paste0(output_prefix, "_stereogene_internal.sh")
-    doCall(runStereogene, args = list(...,
-        track_files = track_files,
-        name_sh = name_sh,
-        name_config = name_config))
-    track_files <- c(paste0(dir_CapR_bg, "/", input_prefix,
-        "_exterior_liftOver.bedGraph"), protein_file)
-    name_sh <- paste0(output_prefix, "_stereogene_exterior.sh")
-    doCall(runStereogene, args = list(...,
-        track_files = track_files,
-        name_sh = name_sh,
-        name_config = name_config))
-    track_files <- c(paste0(dir_CapR_bg, "/", input_prefix,
-        "_stem_liftOver.bedGraph"), protein_file)
-    name_sh <- paste0(output_prefix, "_stereogene_stem.sh")
-    doCall(runStereogene, args = list(...,
-        track_files = track_files,
-        name_sh = name_sh,
-        name_config = name_config))
-    track_files <- c(paste0(dir_CapR_bg, "/", input_prefix,
-        "_multibranch_liftOver.bedGraph"), protein_file)
-    name_sh <- paste0(output_prefix, "_stereogene_multibranch.sh")
-    doCall(runStereogene, args = list(...,
-        track_files = track_files,
-        name_sh = name_sh,
-        name_config = name_config))
-    track_files <- c(paste0(dir_CapR_bg, "/", input_prefix,
-        "_bulge_liftOver.bedGraph"), protein_file)
-    name_sh <- paste0(output_prefix, "_stereogene_bulge.sh")
-    doCall(runStereogene, args = list(...,
-        track_files = track_files,
-        name_sh = name_sh,
-        name_config = name_config))
+    doCall(write_config, args = list(...,
+                                    name_config = name_config,
+                                    chrom_size = chrom_size))
+    for(context in c("hairpin", "stem", "multibranch",
+                     "exterior", "internal", "bulge")){
+        track_files <- c(paste0(dir_CapR_bg, "/", input_prefix, "_",
+                                context, "_liftOver.bedGraph"), protein_file)
+        name_sh <- paste0(output_prefix, "_stereogene_", context, ".sh")
+        doCall(runStereogene, args = list(...,
+                                          track_files = track_files,
+                                          name_sh = name_sh,
+                                          name_config = name_config,
+                                          nShuffle = nShuffle,
+                                          get_error = get_error))
+    }
 }
